55dd1df4d9c04a1e8b0dadcb6efb33595bdc64ea
Ragnorhub <romanpreminin@yandex.ru>
2023-10-22 17:11:34 +0300

    Final release

--- src/main/java/ru/preminin/homeworks/homework4/MainApp.java
+++ src/main/java/ru/preminin/homeworks/homework4/MainApp.java
@@ -1,18 +1,18 @@
 package ru.preminin.homeworks.homework4;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class MainApp {
     public static void main(String[] args) {
-        //stringRepeater(5,"Hello");
-        //System.out.println(Arrays.toString(randomArray()));
-        //sumIfFive(randomArray());
-        //System.out.println(Arrays.toString(randomArray()));
-        //System.out.println(Arrays.toString(arrayOfNumber(5, randomArray())));
-        //System.out.println(Arrays.toString(arrayOfMultiple(5, randomArray())));
-        //comparingArrayParts(randomArray());
+        stringRepeater(5,"Hello");
+        System.out.println(Arrays.toString(randomArray()));
+        sumIfFive(randomArray());
+        System.out.println(Arrays.toString(arrayOfNumber(5, randomArray())));
+        System.out.println(Arrays.toString(arrayOfMultiple(5, randomArray())));
+        comparingArrayParts(randomArray());
         reverseArray(randomArray());
+        arrayIsSorted(randomArray(), (random(5) > 2));
+        arrayEqualiyPoint(randomArray());
     }
 
     public static int random(int range) {
@@ -41,7 +41,6 @@ public class MainApp {
             }
         }
         System.out.println(sum);
-        ;
     }
 
     public static int[] arrayOfNumber(int number, int[] array) {
@@ -73,14 +72,74 @@ public class MainApp {
             System.out.println("Правая часть массива больше");
         }
     }
-    public static void reverseArray (int[] array) {
+
+    public static void reverseArray(int[] array) {
         System.out.println(Arrays.toString(array));
-        for (int i = 0; i < array.length/2; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            array[i] = array[array.length-i-1];
-            array[array.length-i-1] = temp;
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
         System.out.println(Arrays.toString(array));
+    }
+
+    public static void arrayIsSorted(int[] array, boolean isAscending) {
+        System.out.println("Массив: " + Arrays.toString(array));
+        if (array.length <= 1) {
+            System.out.println("Массив состоит из одного элемента, или массив пустой");
+            return;
+        }
+        if (isAscending) {
+            int counter = 0;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] >= array[i + 1]) {
+                    System.out.println("Массив не восходящий.");
+                    break;
+                }
+                counter++;
+                if (counter == array.length - 1) {
+                    System.out.println("Массив восходящий");
+                }
+            }
+        } else {
+            int counter = 0;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] <= array[i + 1]) {
+                    System.out.println("Массив не нисходящий");
+                    break;
+                }
+                counter++;
+                if (counter == array.length - 1) {
+                    System.out.println("Массив нисходящий");
+                }
+            }
+        }
+    }
+
+    public static void arrayEqualiyPoint(int[] arr) {
+        int counter = 0;
+        System.out.println("Массив: " + Arrays.toString(arr));
+        if (arr.length < 2) {
+            System.out.println("В массиве недосаточно элементов для сравнения.");
+            return;
+        }
+        int sumLeft = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumLeft += arr[i];
+            int sumRight = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                sumRight += arr[j];
+            }
+            if (sumLeft == sumRight) {
+                System.out.println("В массиве присутствует точка равенства правой и левой части массива. " +
+                        "Она находится между " + arr[i] + " и " + arr[i + 1]);
+                break;
+            }
+            counter++;
+        }
+        if (counter == arr.length) {
+            System.out.println("В массиве отсутствует точка равенства правой и левой части массива.");
+        }
     }
 }
 
