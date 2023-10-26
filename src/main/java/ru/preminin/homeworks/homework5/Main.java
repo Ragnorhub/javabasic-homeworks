3ed6b05c4a6cdec9fda8f66c72f4781c452d21e7
Ragnorhub <romanpreminin@yandex.ru>
2023-10-26 15:02:11 +0300

    Corrected all comments

--- src/main/java/ru/preminin/homeworks/homework5/Main.java
+++ src/main/java/ru/preminin/homeworks/homework5/Main.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        sumOfPositiveElements(randomTwoDimensionsArray(4));
-        squareInConsole(random(8));
+        //sumOfPositiveElements(randomTwoDimensionsArray(4));
+        //squareInConsole(random(8));
         diaonalsOfArray(randomTwoDimensionsArray(4));
-        findMax(randomTwoDimensionsArray(4));
-        System.out.println(sumSecondElementsInArray(randomTwoDimensionsArray(4)));
+        //findMax(randomTwoDimensionsArray(4));
+        //sumSecondElementsInArray(randomTwoDimensionsArray(4));
     }
 
     public static int random(int range) {
@@ -50,18 +50,17 @@ public class Main {
 
     public static void diaonalsOfArray(int[][] array) {
         System.out.println("Начальный массив: " + Arrays.deepToString(array));
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (i == j) {
-                    array[i][j] = 0;
-                }
-                if (i + j == array.length) {
-                    array[i][j] = 0;
-                }
-            }
+        if (array.length != array[0].length) {
+            System.out.println("Задан не \"квадратный массив\"");
+            return;
+        }
+        for (int i = 0 ; i < array.length; i++) {
+            array[i][i] = 0;
+            array[i][array[i].length - 1 - i] = 0;
         }
         System.out.println("Обновленный массив: " + Arrays.deepToString(array));
     }
+
     public static int findMax(int[][] array) {
         System.out.println("В массиве: " + Arrays.deepToString(array));
         int max = Integer.MIN_VALUE;
@@ -77,14 +76,15 @@ public class Main {
     }
 
     public static int sumSecondElementsInArray(int[][] array) {
-        if (array.length > 1) {
-            int sum = 0;
-            for (int i = 0; i < array[1].length; i++) {
-                sum += array[1][i];
-            }
-            return sum;
-        } else {
+        System.out.println("В массиве: " + Arrays.deepToString(array));
+        if (array.length < 2) {
             return -1;
         }
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i][1];
+        }
+        System.out.println("Сумма вторых элементов массиваравна: " + sum);
+        return sum;
     }
 }
