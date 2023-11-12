1c28ec84edadbfc51c55729ea06c04a4b7d263e2
Ragnor <ragnor@rambler.ru>
2023-11-12 13:17:29 +0300

    Task is complete.

--- src/main/java/ru/preminin/homeworks/homework8/cat/Cat.java
+++ src/main/java/ru/preminin/homeworks/homework8/cat/Cat.java
@@ -1,8 +1,21 @@
 package ru.preminin.homeworks.homework8.cat;
 
+import ru.preminin.homeworks.homework8.plate.Plate;
+
 public class Cat {
     String name;
-    boolean appetite;
+    boolean isSatiety;
 
+    public Cat(String name) {
+        this.name = name;
+        this.isSatiety = false;
+    }
 
+    public void eat(Plate plate) {
+        System.out.println("Кот " + name + " начал кушать.");
+        isSatiety = plate.decreaseFoodToAPlate(26);
+        if (isSatiety) {
+            System.out.println("Кот " + name + " теперь сытый.\n");
+        }
+    }
 }
--- src/main/java/ru/preminin/homeworks/homework8/plate/Plate.java
+++ src/main/java/ru/preminin/homeworks/homework8/plate/Plate.java
@@ -4,6 +4,27 @@ public class Plate {
     int maxFoodAmount;
     int currentAmountOfFood;
 
+    public Plate(int maxFoodAmount, int currentAmountOfFood) {
+        this.maxFoodAmount = maxFoodAmount;
+        this.currentAmountOfFood = currentAmountOfFood;
+    }
 
-
+    public void addFoodToAPlate(int amount) {
+        if (currentAmountOfFood+ amount > maxFoodAmount) {
+            System.out.println("В тарелку " + amount + " еды не влезет. Положите не больше " + (maxFoodAmount-currentAmountOfFood) + " еды.\n");
+        } else {
+            currentAmountOfFood += amount;
+            System.out.println("Добавили в тарелку " + amount + " еды. В тарелке теперь " + currentAmountOfFood + " еды.\n");
+        }
+    }
+    public boolean decreaseFoodToAPlate(int decreaseFood) {
+        if (currentAmountOfFood - decreaseFood < 0) {
+            System.out.println("Коту не хватит еды покушать. Он ушел голодный :,(\n");
+            return false;
+        } else {
+            currentAmountOfFood -= decreaseFood;
+            System.out.println("В тарелке теперь " + currentAmountOfFood + " еды.");
+            return true;
+        }
+    }
 }
