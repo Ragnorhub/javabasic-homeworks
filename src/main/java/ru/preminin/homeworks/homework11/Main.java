58727e028c8a3f168bee779e12876b8108b5662b
Ragnor <ragnor@rambler.ru>
2023-11-30 06:25:33 +0300

    Bug fixes.

--- src/main/java/ru/preminin/homeworks/homework11/Main.java
+++ src/main/java/ru/preminin/homeworks/homework11/Main.java
@@ -35,11 +35,13 @@ public class Main {
         employeeMinAge(list);
     }
 
-    private static void printNames(List<Employee> list) {
+    private static List<String> printNames(List<Employee> list) {
+        List<String> names = new ArrayList<>(list.size());
         for (Employee employee : list) {
-            System.out.println(employee.getName());
+            names.add(employee.getName());
         }
-        System.out.println();
+        System.out.println(names + "\n");
+        return names;
     }
     private static List<Employee> employeesByAge(List<Employee> list, int minAge) {
         List<Employee> newList = new ArrayList<>();
@@ -67,8 +69,8 @@ public class Main {
     }
 
     private static Employee employeeMinAge(List<Employee> list) {
-        int indexMinAge = Integer.MAX_VALUE;
-        int minAge = Integer.MAX_VALUE;
+        int indexMinAge = 0;
+        int minAge = list.get(0).getAge();
         for (int i = 0; i < list.size(); i++) {
             Employee employee = list.get(i);
             if (employee.getAge() < minAge) {
@@ -95,29 +97,5 @@ public class Main {
                 iter.remove();
             }
         }
-        System.out.println(list);*/
-/*    public static void main(String[] args) {
-        List<Integer> values = new ArrayList<>();
-        values.add(5);
-        for (int i = 0; i < 10; i++) {
-            values.add(5);
-        }
-        values.set(1,234);
-        values.set(0,null);
-        System.out.println(values);
-        values.remove(0);
-        System.out.println(values);
-        values.get(0);
-        values.add(3,23343455);
-        values.remove(Integer.valueOf(234));
-        System.out.println(values);
-        values.remove(Integer.valueOf(5));
-        System.out.println(values);
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("A", "AAA", "AA", "AAAA", "A"));
-        Iterator<String> iter = list.iterator();
-        for (String s : list) {
-            if (s.length() > 2) {
-                list.remove(s);
-            }
-        }
+        System.out.println(list);
     }*/
--- src/main/java/ru/preminin/homeworks/homework11/Mathematics.java
+++ src/main/java/ru/preminin/homeworks/homework11/Mathematics.java
@@ -26,7 +26,6 @@ public class Mathematics {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) != number) {
                 list.set(i , number);
-                i--;
             }
         }
         return list;
