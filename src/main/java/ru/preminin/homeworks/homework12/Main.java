7fd2f1abd414eb3ae04c4ab0b1062b867e799ea0
Ragnor <ragnor@rambler.ru>
2023-12-09 16:10:39 +0300

    Homework12 Release candidat.

--- src/main/java/ru/preminin/homeworks/homework12/Main.java
+++ src/main/java/ru/preminin/homeworks/homework12/Main.java
@@ -1,13 +1,24 @@
 package ru.preminin.homeworks.homework12;
 
+/**
+ * Реализуйте класс PhoneBook, который хранит в себе список имен (фио) и телефонных номеров;
+ * Метод add() должен позволять добавлять запись имя-телефон;
+ * Метод find() выполнять поиск номер(-а, -ов) телефона по имени;
+ * Под одним именем может быть несколько телефонов (в случае однофамильцев, или наличии у одного человека нескольких номеров), тогда при запросе такой фамилии должны выводиться все телефоны;
+ * Метод containsPhoneNumber должен проверять наличие телефона в справочнике.
+ **/
+
 public class Main {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addContact("Nata", "+74953530292");
+        phoneBook.add("Nata", "+74953530292");
         System.out.println(phoneBook);
-        phoneBook.addContact("Vlad", "+74953579754");
-        phoneBook.addContact("Vlad", "+74953574542");
+        phoneBook.add("Vlad", "+74953579754");
+        phoneBook.add("Vlad", "+74953574542");
         System.out.println(phoneBook);
-
+        phoneBook.find("Nata");
+        phoneBook.find("Vlad");
+        phoneBook.find("Drtg");
+        phoneBook.containsPhoneNumber("+74953579754");
     }
 }
--- src/main/java/ru/preminin/homeworks/homework12/PhoneBook.java
+++ src/main/java/ru/preminin/homeworks/homework12/PhoneBook.java
@@ -1,13 +1,29 @@
 package ru.preminin.homeworks.homework12;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PhoneBook {
-    Map<Object, Object> phoneBook = new HashMap<>();
+/**
+ * Реализуйте класс PhoneBook, который хранит в себе список имен (фио) и телефонных номеров;
+ * Метод add() должен позволять добавлять запись имя-телефон;
+ * Метод find() выполнять поиск номер(-а, -ов) телефона по имени;
+ * Под одним именем может быть несколько телефонов (в случае однофамильцев, или наличии у одного человека нескольких номеров), тогда при запросе такой фамилии должны выводиться все телефоны;
+ * Метод containsPhoneNumber должен проверять наличие телефона в справочнике.
+ **/
 
-    public void addContact(String name, String phoneNumber) {
-        phoneBook.put(name,phoneNumber);
+public class PhoneBook {
+    Map<String, ArrayList<String>> phoneBook = new HashMap<>();
+
+    public void add(String name, String phoneNumber) {
+        ArrayList<String> phones = phoneBook.get(name);
+        if (phones != null) {
+            phones.add(phoneNumber);
+        } else {
+            phones = new ArrayList<>();
+            phones.add(phoneNumber);
+            phoneBook.put(name, phones);
+        }
     }
 
     @Override
@@ -25,4 +41,25 @@ public class PhoneBook {
         return "PhoneBook=" + phoneBook +
                 '}';
     }
+
+    public void find(String name) {
+        if (phoneBook.containsKey(name)) {
+            System.out.println(phoneBook.get(name));
+        } else {
+            System.out.println("The contact is missing from the list");
+        }
+    }
+
+    public void containsPhoneNumber(String phone) {
+        for (var phones : phoneBook.entrySet()) {
+            for (int i = 0; i < phones.getValue().size(); i++) {
+                if (phone.equals(phones.getValue().get(i))) {
+                    System.out.println("The phone is present in the database");
+                    return;
+                }
+            }
+        }
+        System.out.println("The phone is missing from the database");
+    }
+
 }
